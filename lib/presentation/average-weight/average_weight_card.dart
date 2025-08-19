bb11d8ed57512a72b6ef317d24369e8b6b1ce096
adrianmartinsen <ad.martinsen@gmail.com>
2025-08-19 11:37:39 +1000

    moved all logic for the average weight cubit to the card widget - consider if the remaining code of view can be consolidated with page, and the card widget becomes the view?

---
+++ lib/presentation/average-weight/average_weight_card.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../domain/weighin_model.dart';
+import 'average_weight_cubit.dart';
+
+class AverageWeightCard extends StatelessWidget {
+  final String period; // 'week' or 'month'
+  final String weightType;
+
+  const AverageWeightCard({
+    super.key,
+    required this.period,
+    required this.weightType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    late String title;
+    late List<Weighin> filteredWeighins;
+
+    return BlocBuilder<AverageWeightCubit, List<Weighin>>(
+        builder: (context, weighins) {
+      final lastWeekWeighins =
+          context.read<AverageWeightCubit>().getLastWeekWeighins(weighins);
+      final lastMonthWeighins =
+          context.read<AverageWeightCubit>().getLastMonthWeighins(weighins);
+
+      // When we have more than two cards consider a switch statement instead
+      if (period == 'week') {
+        title = 'Average Weight Last 7 Days';
+        filteredWeighins = lastWeekWeighins;
+      }
+
+      if (period == 'month') {
+        title = 'Average Weight Last 30 Days';
+        filteredWeighins = lastMonthWeighins;
+      }
+
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          color: Colors.grey[800],
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(8, 16, 8, 32),
+            child: Stack(
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        filteredWeighins.isEmpty
+                            ? 'No data'
+                            : '${(filteredWeighins.map((w) => w.weight).reduce((a, b) => a + b) / filteredWeighins.length).toStringAsFixed(2)} $weightType',
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 3,
+                  child: PopupMenuButton<String>(
+                    color: Colors.grey[700],
+                    itemBuilder: (context) => [
+                      const PopupMenuItem<String>(
+                        value: 'edit',
+                        child: Text('Edit'),
+                      ),
+                      const PopupMenuItem<String>(
+                        value: 'delete',
+                        child: Text('Delete'),
+                      ),
+                    ],
+                    child: const Icon(Icons.more_vert, size: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
--- lib/presentation/average-weight/average_weight_view.dart
+++ lib/presentation/average-weight/average_weight_view.dart
@@ -1,43 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../domain/weighin_model.dart';
 import '../settings/settings_cubit.dart';
-import 'average_weight_cubit.dart';
-import 'widgets/average_weight_card.dart';
+import 'average_weight_card.dart';
 
 class AverageWeightView extends StatelessWidget {
   const AverageWeightView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AverageWeightCubit, List<Weighin>>(
-      builder: (context, weighins) {
-        final lastWeekWeighins =
-            context.read<AverageWeightCubit>().getLastWeekWeighins(weighins);
-        final lastMonthWeighins =
-            context.read<AverageWeightCubit>().getLastMonthWeighins(weighins);
-        String weightType;
-        return BlocBuilder<SettingsCubit, String>(
-          builder: (context, weightUnit) {
-            weightType = weightUnit;
+    return BlocBuilder<SettingsCubit, String>(
+      builder: (context, weightUnit) {
+        String weightType = weightUnit;
 
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AverageWeightCard(
-                  period: 'week',
-                  weighins: lastWeekWeighins,
-                  weightType: weightType,
-                ),
-                AverageWeightCard(
-                  period: 'month',
-                  weighins: lastMonthWeighins,
-                  weightType: weightType,
-                ),
-              ],
-            );
-          },
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AverageWeightCard(
+              period: 'week',
+              weightType: weightType,
+            ),
+            AverageWeightCard(
+              period: 'month',
+              weightType: weightType,
+            ),
+          ],
         );
       },
     );
